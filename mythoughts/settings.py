4b42b77a0f8bfa0a1dc4ad64c3468f61a682796e
Ankit <ankitxtowod@gmail.com>
2023-06-29 02:20:11 +0530

    update

--- mythoughts/settings.py
+++ mythoughts/settings.py
@@ -143,7 +143,8 @@ STATIC_ROOT = "../staticfiles/"
 MEDIA_URL = '/media/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_ROOT =BASE_DIR/"media/"
+# MEDIA_ROOT =BASE_DIR/"media/"
+MEDIA_ROOT ="../media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
