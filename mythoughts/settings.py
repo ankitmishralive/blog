e6d13af1293dbd596231be6b71dbe5ec88dd860f
Ankit <ankitxtowod@gmail.com>
2023-06-28 18:09:58 +0530

    update

--- mythoughts/settings.py
+++ mythoughts/settings.py
@@ -132,9 +132,10 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
-STATICFILES_DIRS = [BASE_DIR/"static"]
-# STATIC_ROOT = BASE_DIR/"static/"
-
+# STATICFILES_DIRS = [BASE_DIR/"static" ]
+STATICFILES_DIRS = [BASE_DIR/"static" ]
+STATIC_ROOT = BASE_DIR/"staticfiles"
+# STATIC_ROOT = "../staticfiles/"
 
 
 MEDIA_URL = '/media/'
